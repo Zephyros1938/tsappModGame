27fe5560120e4281e4245fd3da5998d7ff7aaeba
Zephyros1938 <xtrau505true@gmail.com>
2025-03-04 14:04:38 -0600

    Update formatting

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # import zephyros1938.tsapp as tsapp;
 # import zephyros1938.tsappMod as tsappMod;
-import threading as th;
-from zephyros1938.overrides import *;
+import threading as th
+from zephyros1938.overrides import *
+
 
 class Main(metaclass=AutoCastMeta):
     t: Int16 = Int16.MAX_VALUE
-    def __init__(self,*args, **kwargs):
+
+    def __init__(self, *args, **kwargs):
         print(self.t + 1)
         print(self.t.bit_length())
         print(Int16.MAX_VALUE)
